ac3d906cc2616126a980afe97d89c1234e8ff0bc
Daniel Bowden <github@bowden.in>
2014-08-18 16:36:45 +1000

    Revert ""
    This reverts commit 9baaa4cd4d5cb7c2055a0cc70132b714da889db0.

---
+++ continuation.m
@@ -0,0 +1,13 @@
+// Class Continuation
+// Anonymous category to define private methods in an implementation
+//
+// IDECodeSnippetCompletionPrefix: continuation
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 1286EAD0-4DA5-4E98-B346-410DCF5AEE1E
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+
+@interface <#Class Name#> ()
+<#Continuation#>
+@end
