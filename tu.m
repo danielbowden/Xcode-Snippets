99f835d5bc6d171f747dfa60f6087e680f4946c6
Daniel Bowden <github@bowden.in>
2014-08-18 16:36:29 +1000

    Revert ""
    This reverts commit e9065cae35dd326851d6eedd9ecb2e8bda61f650.

---
+++ tu.m
@@ -0,0 +1,10 @@
+// UIControlEventTouchUpInside
+// 
+//
+// IDECodeSnippetCompletionPrefix: tup
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 21DC8ED2-AC7A-4365-821B-54155EE0073C
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+UIControlEventTouchUpInside
